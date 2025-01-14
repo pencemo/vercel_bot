6fbfda2194f90a5ba7e231c0333327eb9738e0fe
unknown <mnmsby4@gmail.com>
2025-01-14 15:44:28 +0530

    webhook setup

--- index.js
+++ index.js
@@ -2,29 +2,28 @@ import { Bot } from "grammy";
 import express from "express"
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
-// import { json } from '@vercel/node';
 dotenv.config();
 
 const token = process.env.BOT_TOKEN
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-// const app = express();
-// app.use(bodyParser.json());
+const app = express();
+app.use(bodyParser.json());
 
 // Create a bot object
 const bot = new Bot(token);
 // bot.init()  
 
-// app.post('/webhook', async (req, res) => {
-//     const update = req.body;
-//     try {
-//       await bot.handleUpdate(update);
-//       res.send('OK');
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).send('Internal Server Error');
-//     }
-//   });
+app.post('/webhook', async (req, res) => {
+    const update = req.body;
+    try {
+      await bot.handleUpdate(update);
+      res.send('OK');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
 bot.use((ctx, next) => {
     const admin = process.env.ADMIN_ID;
@@ -46,31 +45,32 @@ bot.command("start", (ctx) => ctx.reply("Welcome to the bot!"));
 
 
 // Setup a webhook
-export default async (req, res) => {
-    if (req.method === 'POST') {
-      const update = req.body;
-      await bot.init()
-      await bot.handleUpdate(update);
-      res.status(200).send('OK');
-    } else {
-      res.status(405).send('Method Not Allowed');
-    }
-  };
-// const start = async () => {
-//     const url = `${process.env.WEBHOOK_URL}/webhook`;
-//   if (process.env.NODE_ENV !== "development") {
-//     try {
-//         await bot.api.setWebhook(url);
-//         console.log("Bot is running");
-//       } catch (error) {
-//         console.error("Error setting webhook:", error);
-//       }
-//   }
-// }
+// export default async (req, res) => {
+//     if (req.method === 'POST') {
+//       const update = req.body;
+//       await bot.init()
+//       await bot.handleUpdate(update);
+//       res.status(200).send('OK');
+//     } else {
+//       res.status(405).send('Method Not Allowed');
+//     }
+//   };
 
-// start();
+const start = async () => {
+    const url = `${process.env.WEBHOOK_URL}/webhook`;
+  if (process.env.NODE_ENV !== "development") {
+    try {
+        await bot.api.setWebhook(url);
+        console.log("Bot is running");
+      } catch (error) {
+        console.error("Error setting webhook:", error);
+      }
+  }
+}
+
+start();
 bot.start();
 
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// })
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
